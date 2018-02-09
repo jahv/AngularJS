90c34a20037b40a820534533b055aec9b3fa9530
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-02-08 18:11:11 -0600

    Hooks

--- codecraft/src/app/joke-list/joke-list.component.ts
+++ codecraft/src/app/joke-list/joke-list.component.ts
@@ -11,9 +11,9 @@ export class JokeListComponent {
 
   constructor() {
     this.jokes = [
-      new Joke("What did the cheese say when it looked in the mirror?","Hello-Me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’")
+      // new Joke("What did the cheese say when it looked in the mirror?","Hello-Me (Halloumi)"),
+      // new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+      // new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’")
     ];
   }
 
--- codecraft/src/app/joke/joke.component.ts
+++ codecraft/src/app/joke/joke.component.ts
@@ -17,4 +17,43 @@ export class JokeComponent {
     this.jokeDeleted.emit(this.data);
   }
 
+  /**/
+  //Hooks
+
+  constructor() {
+    console.log(`constructor - data is ${this.data}`);
+  }
+
+  ngOnChanges() {
+    console.log(`ngOnChanges - data is ${this.data}`);
+  }
+
+  ngOnInit() {
+    console.log(`ngOnInit - data is ${this.data}`);
+  }
+
+  ngDoCheck() {
+    console.log(`ngDoCheck`);
+  }
+
+  ngAfterContentInit() {
+    console.log(`ngAfterContentInit`);
+  }
+
+  ngAfterContentChecked() {
+    console.log(`ngAfterContentChecked`);
+  }
+
+  ngAfterViewInit() {
+    console.log(`ngAfterViewInit`);
+  }
+
+  ngAfterViewChecked() {
+    console.log(`ngAfterViewChecked`);
+  }
+
+  ngOnDestroy() {
+    console.log(`ngOnDestroy`);
+  }
+
 }

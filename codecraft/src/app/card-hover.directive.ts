509660b084f89c7027089f5f1206775ffb0db626
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-02-22 18:30:35 -0600

    HostListener

--- codecraft/src/app/card-hover.directive.ts
+++ codecraft/src/app/card-hover.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[ccCardHover]'
@@ -8,7 +8,18 @@ export class CardHoverDirective {
   constructor(private el: ElementRef, private renderer: Renderer) {
     console.log(el);
     // el.nativeElement.style.backgroundColor = "gray";
-    renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+    // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+  }
+
+  @HostListener('mouseover')
+  onMouseOver() {
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part, 'display', 'block');
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    let part = this.el.nativeElement.querySelector('.card-text');
+    this.renderer.setElementStyle(part, 'display', 'none');
   }
 
 }
